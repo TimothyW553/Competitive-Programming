949b2e353e40b3eaa831528974c06eda1c3ad452
Timothy Wang <timothywang@crescentschool.org>
2019-08-15 02:06:00 -0400

    Add files via upload

---
+++ CS Academy/Word Permutation.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+ll gcd(ll a, ll b){return b == 0 ? a : gcd(b, a % b);}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+ll fpow(ll b, ll exp, ll mod){if(exp == 0) return 1;ll t = fpow(b,exp/2,mod);if(exp&1) return t*t%mod*b%mod;return t*t%mod;}
+ll divmod(ll i, ll j, ll mod){i%=mod,j%=mod;return i*fpow(j,mod-2,mod)%mod;}
+
+map<string, int> mp;
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n; cin >> n;
+    for(int i = 1; i <= n; i++) {
+        string str; cin >> str;
+        mp[str] = i;
+    }
+    for (map<string,int>::iterator it=mp.begin(); it!=mp.end(); ++it)
+        cout << it->s << " ";
+    return 0;
+}
