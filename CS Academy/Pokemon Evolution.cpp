bc640edbe1e7c8ba91194dec52a9b8f7557405ef
Timothy Wang <timothywang@crescentschool.org>
2019-08-15 23:49:59 -0400

    Add files via upload

---
+++ CS Academy/Pokemon Evolution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+ll gcd(ll a, ll b){return b == 0 ? a:gcd(b, a % b);}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+ll fpow(ll b, ll exp, ll mod){if(exp == 0) return 1;ll t = fpow(b,exp/2,mod);if(exp&1) return t*t%mod*b%mod;return t*t%mod;}
+ll divmod(ll i, ll j, ll mod){i%=mod,j%=mod;return i*fpow(j,mod-2,mod)%mod;}
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll n,m,x,y; cin>>n>>m>>x>>y;
+    ll low=0,high=n;
+    while(low!=high) {
+        int mid=(low+high+1)/2;
+        if(m+(n-mid)*y >= mid*x) {
+            low=mid;
+        } else {
+            high=mid-1;
+        }
+    }
+    cout<<low<<"\n";
+    return 0;
+}
