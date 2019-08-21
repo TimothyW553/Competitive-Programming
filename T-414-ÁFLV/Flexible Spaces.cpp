051aff5bfe533c748bfeacbd3723c7e0db9a0852
Timothy Wang <timothywang@crescentschool.org>
2019-08-21 16:23:21 -0400

    Create Flexible Spaces.cpp

---
+++ T-414-ÁFLV/Flexible Spaces.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+ll gcd(ll a, ll b){return b == 0 ? a : gcd(b, a % b);}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+ll fpow(ll b, ll exp, ll mod){if(exp == 0) return 1;ll t = fpow(b,exp/2,mod);if(exp&1) return t*t%mod*b%mod;return t*t%mod;}
+ll divmod(ll i, ll j, ll mod){i%=mod,j%=mod;return i*fpow(j,mod-2,mod)%mod;}
+
+set<int> st;
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int w, p; cin >> w >> p;
+    int walls[p+2];
+    walls[0] = 0;
+    walls[p+1] = w;
+    for(int i = 1; i <= p; i++) {
+        cin >> walls[i];
+    }
+    for(int i = 0; i < p+2; i++) {
+        for(int j = i+1; j < p+2; j++) {
+            st.insert(abs(walls[i] - walls[j]));
+        }
+    }
+    for(int x : st) {
+        cout << x << " ";
+    }
+    return 0;
+}
