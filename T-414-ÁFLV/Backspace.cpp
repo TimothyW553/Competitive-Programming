6cad6d58a49a6c3c51faedce4c2788d0e7ad7c2a
Timothy Wang <timothywang@crescentschool.org>
2019-08-22 19:05:33 -0400

    Create Backspace.cpp

---
+++ T-414-ÁFLV/Backspace.cpp
@@ -0,0 +1,46 @@
+#pragma GCC optimize "Ofast"
+#pragma GCC optimize "unroll-loops"
+#pragma GCC optimize "omit-frame-pointer"
+#pragma GCC optimize "prefetch-loop-arrays"
+#pragma GCC target "sse,sse2,sse3,sse4,abm,avx,aes,sse4a,sse4.1,sse4.2,mmx,popcnt,tune=native"
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+ll gcd(ll a, ll b){return b == 0 ? a : gcd(b, a % b);}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+ll fpow(ll b, ll exp, ll mod){if(exp == 0) return 1;ll t = fpow(b,exp/2,mod);if(exp&1) return t*t%mod*b%mod;return t*t%mod;}
+ll divmod(ll i, ll j, ll mod){i%=mod,j%=mod;return i*fpow(j,mod-2,mod)%mod;}
+
+stack<char> st;
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    string str; getline(cin, str);
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] == '<') { st.pop(); }
+        else { st.push(str[i]); }
+    }
+    string ans = "";
+    while(!st.empty()) {
+        ans += st.top();
+        st.pop();
+    }
+    reverse(ans.begin(), ans.end());
+    cout << ans << "\n";
+    return 0;
+}
