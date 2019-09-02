3073e926aed0e2c2d46468de6750c5d6885698fe
Timothy Wang <timothywang@crescentschool.org>
2019-09-02 15:54:47 -0400

    Create Rock-Paper-Scissors.cpp

---
+++ Others/Rock-Paper-Scissors.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+ll gcd(ll a, ll b){return b == 0 ? a : gcd(b, a % b);}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+ll fpow(ll b, ll exp, ll mod){if(exp == 0) return 1;ll t = fpow(b,exp/2,mod);if(exp&1) return t*t%mod*b%mod;return t*t%mod;}
+ll divmod(ll i, ll j, ll mod){i%=mod,j%=mod;return i*fpow(j,mod-2,mod)%mod;}
+
+string arr1[101];
+string arr2[101];
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n; cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+    for(int i = 0; i < n; i++) {
+        cin >> arr2[i];
+    }
+    int a = 0, b = 0;
+    for(int i = 0; i < n; i++) {
+        if((arr1[i] == "rock" && arr2[i] == "scissors") || (arr1[i] == "paper" && arr2[i] == "rock") || (arr1[i] == "scissors" && arr2[i] == "paper")) {
+            a++;
+        } else if((arr2[i] == "rock" && arr1[i] == "scissors") || (arr2[i] == "paper" && arr1[i] == "rock") || (arr2[i] == "scissors" && arr1[i] == "paper")) {
+            b++;
+        }
+    }
+    cout << a << " " << b << "\n";
+    return 0;
+}
