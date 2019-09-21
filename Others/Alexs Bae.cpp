dcdb34d9b026d0446cad28cadd953f7e02493c6a
Timothy Wang <timothywang@crescentschool.org>
2019-09-21 16:00:51 -0400

    Create Alexs Bae.cpp

---
+++ Others/Alexs Bae.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return a * b / gcd(a, b); }
+ll fpow(ll b, ll exp, ll mod) { if (exp == 0) return 1; ll t = fpow(b, exp / 2, mod); if (exp & 1) return t * t % mod * b % mod; return t * t % mod; }
+ll divmod(ll i, ll j, ll mod) { i %= mod, j %= mod; return i * fpow(j, mod - 2, mod) % mod; }
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    string s1, s2;
+    getline(cin, s1);
+    getline(cin, s2);
+    string str1 = "", str2 = "";
+    for(char c : s1) {
+        if(c != ' ') { str1 += tolower(c); }
+    }
+    for(char c : s2) {
+        if(c != ' ') { str2 += tolower(c); }
+    }
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+    if(str1 == str2) { cout << "yes" << "\n"; }
+    else { cout << "no" << "\n"; }
+    return 0;
+}
