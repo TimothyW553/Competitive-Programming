489e545f003f62d81961ea4acaa017c96f029499
Timothy Wang <timothywang@crescentschool.org>
2019-10-24 12:17:03 -0400

    Create dmopc16c2p4.cpp

---
+++ Others/dmopc16c2p4.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+ll gcd(ll a, ll b){return b == 0 ? a : gcd(b, a % b);}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+ll fpow(ll b, ll exp, ll mod){if(exp == 0) return 1;ll t = fpow(b,exp/2,mod);if(exp&1) return t*t%mod*b%mod;return t*t%mod;}
+ll divmod(ll i, ll j, ll mod){i%=mod,j%=mod;return i*fpow(j,mod-2,mod)%mod;}
+
+ll binarySearch(ll n) {
+    ll l = 1;
+    ll r = 5000000000;
+    while(l < r) {
+        ll mid = (l + r)/2;
+        ll sum = 0;
+        for(ll i = 5; i <= mid; i *= 5) {
+            sum += mid / i;
+        }
+        if(sum >= n) { r = mid; }
+        else { l = mid + 1; }
+    }
+    return l;
+}
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ll a, b; cin >> a >> b;
+    cout << binarySearch(b+1) - binarySearch(a) << "\n";
+    return 0;
+}
