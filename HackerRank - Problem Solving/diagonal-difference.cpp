a22605947e033a379b231daa7925d3bc79668f0d
Timothy Wang <timothywang@crescentschool.org>
2019-11-27 22:15:44 -0500

    Create diagonal-difference.cpp

---
+++ HackerRank - Problem Solving/diagonal-difference.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+const int MAXN = (1e5 + 5)*3;
+
+ll gcd(ll a, ll b){return b == 0 ? a : gcd(b, a % b);}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+ll fpow(ll b, ll exp, ll mod){if(exp == 0) return 1;ll t = fpow(b,exp/2,mod);if(exp&1) return t*t%mod*b%mod;return t*t%mod;}
+ll divmod(ll i, ll j, ll mod){i%=mod,j%=mod;return i*fpow(j,mod-2,mod)%mod;}
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n; cin >> n;
+    int arr[n][n];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    int primDiag = 0;
+    int secoDiag = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i == j) {
+                primDiag += arr[i][j];
+            }
+            if(i + j == n-1) {
+                secoDiag += arr[i][j];
+            }
+        }
+    }
+    cout << abs(primDiag - secoDiag) << "\n";
+    return 0;
+}
