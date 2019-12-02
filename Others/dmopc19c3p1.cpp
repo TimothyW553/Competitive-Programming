edb720fa7d2e21168c7c2b73e424f920fdee5039
Timothy Wang <timothywang@crescentschool.org>
2019-12-02 17:14:10 -0500

    Create dmopc19c3p1.cpp

---
+++ Others/dmopc19c3p1.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+ll gcd(ll a, ll b){return b == 0 ? a : gcd(b, a % b);}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+ll fpow(ll b, ll exp, ll mod){if(exp == 0) return 1;ll t = fpow(b,exp/2,mod);if(exp&1) return t*t%mod*b%mod;return t*t%mod;}
+ll divmod(ll i, ll j, ll mod){i%=mod,j%=mod;return i*fpow(j,mod-2,mod)%mod;}
+
+unordered_map<int, int> m;
+vector<int> modes;
+int freq = -MAX;
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n; cin >> n;
+    for(int i = 0; i < n; i++) {
+        int num; cin >> num;
+        if(m.find(num) == m.end()) {
+            m[num] = 0;
+        } else {
+            m[num]++;
+        }
+    }
+    for(auto x : m) {
+        int num = x.f;
+        int val = x.s;
+        if(val > freq) {
+            freq = val;
+            modes.clear();
+            modes.pb(num);
+        } else if(val == freq) {
+            modes.pb(num);
+        }
+    }
+    sort(modes.begin(), modes.end());
+    for(int i = 0; i < modes.size(); i++) {
+        cout << modes[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
