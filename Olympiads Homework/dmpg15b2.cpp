7c4e041799d9e092a1e01fabc4657d8454c632ee
Timothy Wang <timothywang@crescentschool.org>
2019-12-07 11:43:48 -0500

    Create dmpg15b2.cpp

---
+++ Olympiads Homework/dmpg15b2.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f first
+#define s second
+#define pb push_back
+#define FILL(a, b) memset(a, b, sizeof(a))
+
+typedef long long int ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+const int INF = 0x3f3f3f3f;
+const ll INFL = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+ll gcd(ll a, ll b){return b == 0 ? a : gcd(b, a % b);}
+ll lcm(ll a, ll b){return a*b/gcd(a,b);}
+ll fpow(ll b, ll exp, ll mod){if(exp == 0) return 1;ll t = fpow(b,exp/2,mod);if(exp&1) return t*t%mod*b%mod;return t*t%mod;}
+ll divmod(ll i, ll j, ll mod){i%=mod,j%=mod;return i*fpow(j,mod-2,mod)%mod;}
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int trunk[3];
+    int car[3];
+    cin >> trunk[0] >> trunk[1] >> trunk[2];
+    cin >> car[0] >> car[1] >> car[2];
+    sort(trunk, trunk+3);
+    sort(car, car+3);
+    bool flag = true;
+    for(int i = 0; i < 3; i++) {
+        if(trunk[i] > car[i]) {
+            flag = false;
+            break;
+        }
+    }
+    cout << (flag ? "Y" : "N") << "\n";
+    return 0;
+}
