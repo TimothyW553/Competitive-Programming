71ff486b02da97b40ef3c16dd9c18375eb99abd9
Timothy Wang <timothywang@crescentschool.org>
2019-12-07 20:07:14 -0500

    Create waterloow2017a.java

---
+++ Olympiads Homework/waterloow2017a.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class waterloow2017a {
+
+    static BufferedReader br;
+    static PrintWriter out;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(new OutputStreamWriter(System.out));
+        int n = readInt();
+        System.out.println(n * (n-1)); // for every top and pant (except the same colour)
+    }
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
