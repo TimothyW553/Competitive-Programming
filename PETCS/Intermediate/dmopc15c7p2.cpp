6b3f29becdf28b6557a9707c985d0c9d2ac2d3a0
Timothy Wang <timothywang@crescentschool.org>
2020-07-09 23:13:58 -0400

    Create dmopc15c7p2.cpp

---
+++ PETCS/Intermediate/dmopc15c7p2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int cnt = 0;
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    string s; 
+    while(cin >> s) {
+        cnt++;
+    }
+    cout << cnt << "\n";
+    return 0;
+}
