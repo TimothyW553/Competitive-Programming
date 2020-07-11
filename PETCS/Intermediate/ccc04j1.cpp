d7db0cc25a3edb49a1780374e7e1531d314a4aae
Timothy Wang <timothywang@crescentschool.org>
2020-07-11 13:28:27 -0400

    Create ccc04j1.cpp

---
+++ PETCS/Intermediate/ccc04j1.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int main() {
+    scanf("%d", &N);
+    printf("The largest square has side length %.0f.\n", floor(sqrt(N)));
+    return 0;
+}
