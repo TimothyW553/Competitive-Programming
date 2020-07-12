243c4a59d07ef91891497a34797aec0423b24a4a
Timothy Wang <timothywang@crescentschool.org>
2020-07-11 23:25:50 -0400

    Create infinity.cpp

---
+++ PETCS/Intermediate/infinity.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int A, B;
+int main() {
+    scanf("%x%x", &A, &B);
+    if(A+B <= 1061109567) {
+        printf("No\n");
+    } else {
+        printf("Yes\n");
+    }
+    return 0;
+}
